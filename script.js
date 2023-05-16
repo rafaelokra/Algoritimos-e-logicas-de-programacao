e49118f278c66276e1cb79a344fef2251c64c622
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-05-15 23:36:22 -0300

    treinando

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Econtre a soluçao ao problema:
+
+Pergunte o nome do usuario e escreva a mensagem:
+  "Ola , [nome do usuario]"
+
+  String = Cadeia de caracteres
+  
+  */
+
+
+  
+  let nome = prompt("Qual o seu nome")
+  alert("Ola, " + nome)
